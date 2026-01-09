cd20010376b237a4d9a8c471b0d30946b3c30c99
Thomas Andrianto <thomasandrianto1980@gmail.com>
2026-01-09 21:38:39 +0700

    Add published scope and user relationship to Post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,11 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory;
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function scopePublished(Builder $query): Builder
+    {
+        return $query
+            ->where('is_draft', false)
+            ->whereNotNull('published_at')
+            ->where('published_at', '<=', now());
+    }
 }
